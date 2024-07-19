09bec5a6f8389485a0acce9c3f0631e5e630baf7
unknown <arante.emmanuel2017@gmail.com>
2024-07-20 00:05:32 +0800

    Key Events

---
+++ KeyEvents/index.js
@@ -0,0 +1,45 @@
+// window.alert("Key Events in JavaScript! ");
+
+const myBox = document.getElementById("myBox"); 
+
+const moveAmount = 10;
+
+let x = 0;
+let y = 0;
+
+
+document.addEventListener("keydown", event => {
+  myBox.textContent = "😁";
+  myBox.style.backgroundColor = "tomato";
+})
+
+document.addEventListener("keyup", event => {
+  myBox.textContent = "😊";
+  myBox.style.backgroundColor = "violet";
+})
+
+
+document.addEventListener("keydown", event => {
+  if(event.key.startsWith("Arrow")) {
+
+    // event.preventDefault();
+
+    switch(event.key) {
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDown":
+        y += moveAmount;
+        break;
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+    myBox.style.top = `${y}px`;
+    myBox.style.left = `${x}px`;
+
+  }
+})
